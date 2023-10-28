691603d525e196645bf522821ef0e12b422b6cff
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-10-28 20:43:03 +0300

    add singleton

---
+++ src/main/java/org/example/DbSingleton.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbSingleton {
+
+    private Connection conn = null;
+
+    private static class LazyHolder {
+        static final DbSingleton INSTANCE = new DbSingleton();// daca facem asa e lazy si thread safe => va fi creata doar daca este chemata
+    }
+
+    private DbSingleton(){
+        try{
+            String jdbcURL = "jdbc:h2:mem:test";
+            conn = DriverManager.getConnection(jdbcURL);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static DbSingleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    public Connection getConnection(){ // e thread safe pentru ca nu putem obtine o instanta fara a folosi getInstance care va initialzia tot prin lazy holder
+        return conn;
+    }
+}
---
+++ src/main/java/org/example/DbSingletonDemo.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbSingletonDemo {
+
+    public static void main(String[] args) throws SQLException {
+        DbSingleton dbSingletonInstance = DbSingleton.getInstance();
+        System.out.println(dbSingletonInstance);
+
+        Connection conn = dbSingletonInstance.getConnection();
+
+        Statement statement = conn.createStatement();
+
+        statement.execute("create table students (ID int primary key , name varchar(65))");
+
+        System.out.println("Create table students");
+
+        int rows = statement.executeUpdate("insert into students (ID, name) values (1, 'Cosmin')");
+        System.out.println(rows);
+
+        if(rows > 0){
+            System.out.println("inserted a new row.");
+        }
+
+        conn.close();
+    }
+}
